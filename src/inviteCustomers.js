da67239747b5a9187b5ac0a5856b278d216a290c
Adam Hutchinson <adjohu@gmail.com>
2017-08-31 01:39:19 +0100

    added tests for inviteCustomers

--- src/inviteCustomers.js
+++ src/inviteCustomers.js
@@ -1,27 +1,22 @@
-import customers from './customers.json';
-import {Point, calculateDistanceOnEarth} from './location.js';
+import {Point, calculateDistanceOnEarth} from './location';
 
 // Office location
-const office = new Point(53.339428, -6.257664);
+const OFFICE = new Point(53.339428, -6.257664);
 
 // Max customer distance to invite
 const MAX_DISTANCE = 100; // KM
 
-const nearbyCustomers = customers.filter(({latitude, longitude}) => {
-  const location = new Point(latitude, longitude);
-  return calculateDistanceOnEarth(office, location) <= MAX_DISTANCE;
-});
+export default function inviteCustomers(
+  customers,
+  maxDistance = MAX_DISTANCE,
+  location = OFFICE
+) {
+  const nearbyCustomers = customers.filter(({latitude, longitude}) => {
+    const customerLocation = new Point(latitude, longitude);
+    return calculateDistanceOnEarth(location, customerLocation) <= maxDistance;
+  });
 
-// Overkill as it's only used once but hey
-const pick = fields => obj => {
-  return fields.reduce((result, field) => {
-    if (Object.hasOwnProperty.call(obj, field)) {
-      result[field] = obj[field];
-    }
-    return result;
-  }, {});
-};
-
-const sortedCustomers = nearbyCustomers
-  .sort((a, b) => a.user_id > b.user_id)
-  .map(pick(['user_id', 'name']));
+  return nearbyCustomers
+    .sort((a, b) => a.user_id > b.user_id)
+    .map(({user_id, name}) => ({user_id, name}));
+}
--- src/inviteCustomers.spec.js
+++ src/inviteCustomers.spec.js
@@ -1,1 +1,40 @@
+import test from 'tape';
 import inviteCustomers from './inviteCustomers';
+import {Point} from './location';
+import customers from './customers.json';
+
+const OFFICE = new Point(53.339428, -6.257664);
+
+const eligibleCustomers = [
+  {
+    user_id: 2,
+    name: 'test2',
+    latitude: OFFICE.latitude,
+    longitude: OFFICE.longitude
+  },
+  {
+    user_id: 1,
+    name: 'test',
+    latitude: OFFICE.latitude,
+    longitude: OFFICE.longitude
+  }
+];
+
+test('inviteCustomers should find 13 eligible customers in customers.json', t => {
+  const result = inviteCustomers(customers);
+  t.true(result.length === 13);
+  t.end();
+});
+
+test('inviteCustomers should sort customers by user_id', t => {
+  const result = inviteCustomers(eligibleCustomers);
+  t.true(result[0].user_id === 1);
+  t.true(result[1].user_id === 2);
+  t.end();
+});
+
+test('inviteCustomers should only return name and user_id', t => {
+  const result = inviteCustomers(eligibleCustomers);
+  t.same(Object.keys(result[0]), ['user_id', 'name']);
+  t.end();
+});
